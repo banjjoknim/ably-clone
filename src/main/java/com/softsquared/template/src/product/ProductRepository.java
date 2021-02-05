937222bdf61e6b46797cefb7c7a27be401330f31
banjjoknim <zhfxmtkachdt@daum.net>
2021-02-06 07:22:38 +0900

    feature : 전체 상품 조회 기능 추가

---
+++ src/main/java/com/softsquared/template/src/product/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.softsquared.template.src.product;
+
+import com.softsquared.template.DBmodel.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
