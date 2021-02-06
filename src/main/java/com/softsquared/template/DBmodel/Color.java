106da129733e99374593293a06d506ad2d73dcc8
banjjoknim <zhfxmtkachdt@daum.net>
2021-02-07 03:42:09 +0900

    feature : 상품 조회시 상세 필터링을 위한 Entity 클래스 추가

---
+++ src/main/java/com/softsquared/template/DBmodel/Color.java
@@ -0,0 +1,27 @@
+package com.softsquared.template.DBmodel;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@NoArgsConstructor
+@Getter
+@Entity
+@Table(name = "Color")
+public class Color {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Builder
+    public Color(String name) {
+        this.name = name;
+    }
+}
