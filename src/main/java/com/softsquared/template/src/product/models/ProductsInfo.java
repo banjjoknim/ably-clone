1d7edd52630b853ad433a2da1a9fabaced63a366
banjjoknim <zhfxmtkachdt@daum.net>
2021-02-09 22:51:55 +0900

    style : celebName 에서 marketName 으로 필드명 변경

--- src/main/java/com/softsquared/template/src/product/models/ProductsInfo.java
+++ src/main/java/com/softsquared/template/src/product/models/ProductsInfo.java
@@ -9,17 +9,17 @@ public class ProductsInfo {
     private Long productId;
     private Integer discountRate;
     private Integer price;
-    private String celebName;
+    private String marketName;
     private String productName;
     private Long purchaseCount;
     private Long reviewCount;
 
     @QueryProjection
-    public ProductsInfo(Long productId, Integer discountRate, Integer price, String celebName, String productName, Long purchaseCount, Long reviewCount) {
+    public ProductsInfo(Long productId, Integer discountRate, Integer price, String marketName, String productName, Long purchaseCount, Long reviewCount) {
         this.productId = productId;
         this.discountRate = discountRate;
         this.price = price;
-        this.celebName = celebName;
+        this.marketName = marketName;
         this.productName = productName;
         this.purchaseCount = purchaseCount;
         this.reviewCount = reviewCount;
